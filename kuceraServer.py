6c79fa0817977f7daa8d0c33672e6550ce3c6527
oggo <oggokoko@gmail.com>
2016-08-13 11:16:01 +0200

    Flask support added. Application completed

---
+++ kuceraServer.py
@@ -0,0 +1,160 @@
+#!/usr/bin/env python
+# encoding: utf-8
+'''kuceraServer is the server side application to support the kucera website. 
+@author:     oggo
+@copyright:  1998 c ivanov Consulting Inc.'''
+
+import sys
+import datetime
+import json
+from flask import Flask, flash, render_template, redirect, request, Response
+from flask_sqlalchemy import SQLAlchemy
+
+app= Flask(__name__)
+app.secret_key = 'kucera_very_secret_key_zzzhghrtebrr87011'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://adminqpXQxYv:C5yZDCjUwZaU@127.8.30.2/python'
+db= SQLAlchemy(app)
+reload(sys)
+sys.setdefaultencoding('utf-8')
+
+#Models
+class Article(db.Model):
+  id= db.Column(db.Integer, primary_key=True)
+  htmlId= db.Column(db.String(100))
+  title = db.Column(db.String(80))
+  body = db.Column(db.Text)
+  created= db.Column(db.DateTime, default=datetime.datetime.utcnow)
+  lastUpdated= db.Column(db.DateTime, default=datetime.datetime.utcnow)
+
+  def __init__(self, pTitle, pHtmlId, pBody, pCreated, pLastUpdated, pId= None):
+    self.id = pId
+    self.title = pTitle
+    self.htmlId= pHtmlId
+    self.body = pBody
+    self.created= pCreated
+    self.lastUpdated= pLastUpdated
+
+#internal utils
+def __getResponse(pBase, pStatus):
+  resp= Response(pBase, pStatus)
+  headers= resp.headers
+  headers['Access-Control-Allow-Origin'] = '*'
+  return resp
+
+#Controllers
+#Admin
+@app.route("/api/article/create", methods=["GET", "POST"])
+def createArticle():
+  if request.method == "GET":
+    return render_template("createArticle.html")
+  else:
+    title= request.form["title"]
+    htmlId= request.form["htmlId"]
+    body= request.form["body"].replace('\n', '<br/>').replace('\r', '')
+    created= datetime.datetime.now()
+    lastUpdated= datetime.datetime.now()
+    article= Article(pTitle=title, pHtmlId=htmlId, pBody=body, pCreated=created, pLastUpdated=lastUpdated)
+    db.session.add(article)
+    db.session.commit()
+    flash("Article successfully created!")
+    return redirect("/")
+
+@app.route("/api/article/findForUpdate/<int:pId>")
+def findArticleForUpdate(pId):
+  article= Article.query.get(pId)
+  #article.body.replace('<br/>', '\n')
+  article.body= article.body.replace('<br/>', '\n')
+  return render_template("updateArticle.html", article=article)
+
+@app.route("/api/article/update", methods=["GET", "POST"])
+def updateArticle():
+  localId= request.form["id"]
+  article= Article.query.filter_by(id=localId).first()
+  article.title= request.form["title"]
+  article.htmlId= request.form["htmlId"]
+  article.body= request.form["body"].replace('\n', '<br/>').replace('\r', '')
+  article.lastUpdated= datetime.datetime.now()
+  db.session.commit()
+  flash("Article successfully updated!")
+  return redirect("/")
+
+@app.route('/api/article/deleteById/<int:pId>')
+def deleteArticleTitleById(pId):
+  # show the post with the given id, the id is an integer
+  Article.query.filter_by(id=pId).delete()
+  db.session.commit()
+  return redirect("/")
+
+@app.route("/api/article/viewAll", methods=['GET'])
+def viewAllArticle():
+  '''Lists all the articles'''
+  allArticle= Article.query.all()
+  print "DEBUG: allArticle size: {}".format(len(allArticle))
+  return render_template("viewAllArticle.html", allArticle=allArticle)
+
+@app.route('/')
+def home():
+  #return 'Hello, World!'
+  return render_template("home.html")
+
+#API
+@app.route('/api/article/title/findById/<int:pId>')
+def findArticleTitleById(pId):
+    # show the post with the given id, the id is an integer
+    article= Article.query.filter_by(id=pId).first()
+    if article:
+      return article.title
+    else:
+      return Response("Not found", status=404)
+  
+@app.route('/api/article/title/findByHtmlId/<string:pHtmlId>')
+def findArticleTitleByHtmlId(pHtmlId):
+    # show the post with the given id, the id is an integer
+    article= Article.query.filter_by(htmlId=pHtmlId).first()
+    if article:
+      resp= Response(article.title, status=200)
+      headers= resp.headers
+      headers['Access-Control-Allow-Origin'] = '*'
+      return resp
+    else:
+      return Response("Not found", status=404)
+  
+@app.route('/api/article/body/findById/<int:pId>')
+def findArticleBodyById(pId):
+    # show the post with the given id, the id is an integer
+    article= Article.query.filter_by(id=pId).first()
+    if article:
+      return __getResponse(article.body, 200)
+    else:
+      return Response("Not found", status=404)
+  
+@app.route('/api/article/body/findByHtmlId/<string:pHtmlId>')
+def findArticleBodyByHtmlId(pHtmlId):
+    # show the post with the given id, the id is an integer
+    article= Article.query.filter_by(htmlId=pHtmlId).first()
+    
+    if article:
+      return __getResponse("{\"articles\" : [\"dzhhhh\", \"brom\"]}", 200)
+    else:
+      return Response("Not found", status=404)
+  
+@app.route('/api/article/body/getAll')
+def getAllArticleBody():
+    # show the post with the given id, the id is an integer
+    allArticle= Article.query
+    resp= "{\"articles\" : ["
+    print "DEBUG: resp is: {}".format(resp)
+    if allArticle:
+      for article in allArticle:
+        print "DEBUG: current article is: {}".format(str(article))
+        resp+= "{\"htmlId\" : \"%s\", \"body\" : \"%s\"}, " % (article.htmlId, article.body)
+        print "DEBUG: resp is: {}".format(resp)
+  
+      resp= resp[:-2] + "]}"
+      print "DEBUG: resp will be: %s" % (str(resp))
+      return __getResponse(resp, 200)
+    else:
+      return Response("Not found", status=404)
+  
+if __name__ == "__main__":
+    app.run(debug=True)
--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
 from setuptools import setup
 
-setup(name='YourAppName',
+setup(name='kucera server api',
       version='1.0',
       description='OpenShift App',
-      author='Your Name',
-      author_email='example@example.com',
-      url='http://www.python.org/sigs/distutils-sig/',
+      author='oggo',
+      author_email='oggokoko@gmail.com',
+      url='http://geekline.org',
 #      install_requires=['Django>=1.3'],
      )
