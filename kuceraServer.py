f678474c79ebca52d110197443b6d65227f6de86
oggo <oggokoko@gmail.com>
2017-03-05 20:56:30 +0100

    issue #22 Access-Control-Allow-Origin set to dddd.de and kucera.biz

--- kuceraServer.py
+++ kuceraServer.py
@@ -13,9 +13,8 @@ from flask_sqlalchemy import SQLAlchemy
 
 app= Flask(__name__)
 app.secret_key = 'kucera_very_secret_key_zzzhghrtebrr87011'
-#app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://adminqpXQxYv:C5yZDCjUwZaU@127.8.30.2/python'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://adminqpXQxYv:C5yZDCjUwZaU@127.8.30.2/python'
 #app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:<put the right one>@localhost/python'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:e6kobe6ko@localhost/python'
 db= SQLAlchemy(app)
 reload(sys)
 sys.setdefaultencoding('utf-8')
