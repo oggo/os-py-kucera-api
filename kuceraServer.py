5db93f166d565b8fdc77553d74f333b2e81bc9af
oggo <oggokoko@gmail.com>
2017-05-11 21:41:23 +0200

    issue #24 - test 4

--- kuceraServer.py
+++ kuceraServer.py
@@ -76,14 +76,14 @@ def __getResponse(pBase, pStatus):
   resp= Response(pBase, pStatus)
   headers= resp.headers
   print "DEBUG: request.environ['HTTP_ORIGIN'] is: {}".format(request.environ['HTTP_ORIGIN'])
-  if request.http_origin in ['http://kucera.biz', 'http://www.kucera.biz', 'http://www.dddd.de', 'http://dddd.de']:
-    headers['Access-Control-Allow-Origin'] = '*'
-    print "DEBUG: request.http_origin is IN: ".format(request.http_origin)
-  else:
-    headers['Access-Control-Allow-Origin'] = '*'
-    print "DEBUG: request.http_origin is OUT: ".format(request.http_origin)
+#   if request.http_origin in ['http://kucera.biz', 'http://www.kucera.biz', 'http://www.dddd.de', 'http://dddd.de']:
+#     headers['Access-Control-Allow-Origin'] = '*'
+#     print "DEBUG: request.http_origin is IN: ".format(request.http_origin)
+#   else:
+#     headers['Access-Control-Allow-Origin'] = '*'
+#     print "DEBUG: request.http_origin is OUT: ".format(request.http_origin)
 
-#  headers['Access-Control-Allow-Origin'] = '*'
+  headers['Access-Control-Allow-Origin'] = '*'
 #   if request.path[:11] in ['http://kucera.biz', 'http://www.kucera.biz', 'www.dddd.de']:
 #     headers['Access-Control-Allow-Origin'] = '*'
 #   else:
